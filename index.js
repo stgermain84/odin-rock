55bdc24cf63f60b5793f803af1d86b8f20f6da52
stgermain84 <137921007+stgermain84@users.noreply.github.com>
2023-07-08 15:21:32 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,80 @@
+console.log('I\'m Sailor Moon');
+
+const options = ["rock", "paper", "scissors"];
+
+function getComputerChoice(){
+    const choice = options[Math.floor((Math.random() * options.length))];
+    return choice;
+}
+
+function checkWinner(playerSelection, computerSelection){
+    if(playerSelection == computerSelection){
+        return "Tie";
+    }
+    else if (
+        (playerSelection == "rock" && computerSelection == "scissors") ||
+        (playerSelection == "scissors" && computerSelection == "paper") ||
+        (playerSelection == "paper" && computerSelection == "rock")
+    ){
+        return "Player";
+    }
+    else {
+        return "Computer";
+    }
+}
+
+function playRound(playerSelection, computerSelection){
+    const result = checkWinner(playerSelection, computerSelection);
+        if(result == "Tie"){
+            return "It's a Tie!"
+        }
+        else if(result == "Player"){
+            return `You Win! ${playerSelection} beats ${computerSelection}`;
+        }
+        else {
+            return `You Lose! ${computerSelection} beats ${playerSelection}`;
+        }
+    }
+
+function getPlayerChoice(){
+    let validatedInput = false;
+    while(validatedInput == false){
+        const choice = prompt("Rock Paper Scissors");
+        if(choice == null){
+            continue;
+        }
+        const choiceInLower = choice.toLowerCase();
+        if(options.includes(choiceInLower)){
+            validatedInput = true;
+            return choiceInLower;
+        }
+    }
+}
+
+function game(){
+    let scorePlayer = 0;
+    let scoreComputer = 0;
+    console.log("Welcome!");
+    for (let i = 0; i < 5; i++){
+        const playerSelection = getPlayerChoice();
+        const computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection));
+        if(checkWinner(playerSelection, computerSelection) == "Player"){
+            scorePlayer++;
+        }
+        else if(checkWinner(playerSelection, computerSelection) == "Computer"){
+            scoreComputer++;
+        }
+    }
+    if (scorePlayer > scoreComputer){
+        console.log("You win!");
+    }
+    else if (scorePlayer < scoreComputer){
+        console.log("You lose!");
+    }
+    else {
+        console.log("No winner!");
+    }
+}
+
+game();
